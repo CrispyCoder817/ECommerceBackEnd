74e9a010c778b942abfa4986d386309f2be363e5
Brandon G.M <brandongmerritt@gmail.com>
2021-04-19 19:06:10 -0400

    Destroy updated

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,69 +1,69 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
     include: [
       {
         model: Category,
-        attributes: ['id', 'category_name']
+        attributes: ["id", "category_name"],
       },
-      {model: Tag,
-      attributes: ['id', 'tag name']
-    }
-   ]
+      { model: Tag, attributes: ["id", "tag name"] },
+    ],
   })
-  .then(dbProductData => res.json(dbProductData))
-  .catch(err);
+    .then((dbProductData) => res.json(dbProductData))
+    .catch(err);
   res.status(500).json(err);
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   Product.findOne({
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
     include: [
       {
         model: Category,
-        attributes: ['id', 'category_name']
+        attributes: ["id", "category_name"],
       },
       {
         model: Tag,
-        attributes: ['id', 'tag_name']
-      }
-    ]
+        attributes: ["id", "tag_name"],
+      },
+    ],
   })
-    .then(dbProductData => {
+    .then((dbProductData) => {
       if (!dbProductData) {
-        res.status(404).json({ message: 'No product found with this id'}); 
-        return; 
+        res
+          .status(404)
+          .json({ message: "This is not the product you are looking for" });
+        return;
       }
       res.json(dbProductData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(419).json(err);
     });
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   Product.create({
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    category_id: req.body.category_id,
-    tagIds: req.body.tag_id
-  })
+    // category_id: req.body.category_id,
+    tagIds: req.body.tag_id,
+  });
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -95,7 +95,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -136,8 +136,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: "This product does not exist" });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(419).json(err);
+    });
 });
 
 module.exports = router;
