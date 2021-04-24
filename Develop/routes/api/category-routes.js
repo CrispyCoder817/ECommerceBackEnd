8edfafe6b9602f78736494c08b8330adcf250bcb
Brandon G.M <brandongmerritt@gmail.com>
2021-04-24 13:54:51 -0400

    last push

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -10,14 +10,14 @@ router.get("/", (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ["id", "product_name", "price", "stock"],
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
       },
     ],
   })
     .then((dbCategoryData) => res.json(dbCategoryData))
     .catch((err) => {
       console.log(err);
-      res.status(419).json(err);
+      res.status(500).json(err);
     });
 });
 
@@ -37,7 +37,9 @@ router.get("/:id", (req, res) => {
   })
     .then((dbCategoryData) => {
       if (!dbCategoryData) {
-        res.status(404).json({ message: "No category; Use the power of OverstackFlow" });
+        res
+          .status(404)
+          .json({ message: "No category; Use the power of OverstackFlow" });
         return;
       }
       res.json(dbCategoryData);
@@ -48,16 +50,16 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   // create a new category
   Category.create({
-    category_name: req.body.category_name,
+    category_name: req.body.category_name
   })
-    .then((dbCategoryData) => res.json(dbCategoryData))
-    .catch((err) => {
-      console.log(err);
-      res.status(419).json(err);
-    });
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
 });
 
 router.put("/:id", (req, res) => {
